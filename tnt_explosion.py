224e830b9d3a6226182dd0153cac437ab1e09c0f
im <poson_hemp@yahoo.co.jp>
2022-10-27 08:13:01 +0900

    refactor

--- tnt_explosion.py
+++ tnt_explosion.py
@@ -1,25 +1,36 @@
 """Set explodable TNT blocks.
 
-- When struck with a sword, it detonates.
-- ref: https://gangannikki.hatenadiary.jp/entry/2019/04/27/000000
+When struck with a sword, it detonates.
+- ref
+    - https://gangannikki.hatenadiary.jp/entry/2019/04/27/000000
+    - http://igarashi-systems.com/sample/translation/raspberry-pi/usage/minecraft.html
 """
 from mcpi.minecraft import Minecraft
 
-# connect to minecraft
-mc = Minecraft.create()
-mc.postToChat("TNT init...")
+TNT_BLOCK = 46
+TNT_EXPLODABLE = 1
 
-# get player's position
-x, y, z = mc.player.getPos()
 
-# set TNT block
-mc.setBlocks(
-    x + 1,
-    y + 1,
-    z + 1,
-    x + 3,
-    y + 3,
-    z + 3,
-    46,
-    1
-)
+def main():
+    # connect to minecraft
+    mc = Minecraft.create()
+    mc.postToChat("TNT init...")
+
+    # get player's position
+    x, y, z = mc.player.getPos()
+
+    # set TNT block
+    mc.setBlocks(
+        x + 1,
+        y + 1,
+        z + 1,
+        x + 3,
+        y + 3,
+        z + 3,
+        TNT_BLOCK,
+        TNT_EXPLODABLE
+    )
+
+
+if __name__ == "__main__":
+    main()
